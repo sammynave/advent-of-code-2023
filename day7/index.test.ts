63c22052012709726d67ecdd3ae4bd75eaee78ff
Sammy Nave <sammy.nave@gmail.com>
2023-12-07 09:39:38 -0500

    day7 temp

---
+++ day7/index.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "vitest";
+import { part1, part2 } from "./index.js";
+import { input } from "./input.js";
+
+const exampleInput = 0;
+
+test("part1 - the example input", () => {
+  expect(part1(exampleInput)).toBe(0);
+});
+
+// test("part1 - the actual input", () => {
+//   expect(part1(input())).toBe(0);
+// });
+
+// test("part2 - the example input", () => {
+//   expect(part2(exampleInput)).toBe(0);
+// });
+
+// test("part2 - actual input", () => {
+//   expect(part2(input())).toBe(0);
+// });
---
+++ day7/index.ts
@@ -0,0 +1,10 @@
+const cards = ["A", "K", "Q", "J", "T", 9, 8, 7, 6, 5, 4, 3, 2];
+
+const handTypes = {
+  "five-of-a-kind": (hand) => {},
+  "four-of-a-kind": (hand) => {},
+};
+
+export const part1 = (input) => input;
+
+export const part2 = (input) => {};
