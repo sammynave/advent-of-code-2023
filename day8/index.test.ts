07c39f170e1114c729a36ff503e3d856aeaa1c74
sammy <sammy@MacBook-Air.localdomain>
2023-12-08 12:01:51 -0500

    part2 - inefficient

--- day8/index.test.ts
+++ day8/index.test.ts
@@ -27,10 +27,21 @@ test("part1 - the actual input", () => {
 	expect(part1(input())).toBe(15989);
 });
 
-// test("part2 - the example input", () => {
-//   expect(part2(exampleInput)).toBe(0);
-// });
+const exampleInputPart2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+test("part2 - the example input", () => {
+	expect(part2(exampleInputPart2)).toBe(6);
+});
 
 // test("part2 - actual input", () => {
-//   expect(part2(input())).toBe(0);
+// 	expect(part2(input())).toBe(0);
 // });
--- day8/index.ts
+++ day8/index.ts
@@ -16,7 +16,7 @@ const formatNodeDirections = ([node, directions]: [
 	directions.replace(/\(|\)/g, "").split(", ") as Directions,
 ];
 
-export const part1 = (input) => {
+const parse = (input): [Instructions, NodeDirections[]] => {
 	const [instructionsStr, nodeLeftRights]: [string, string] =
 		input.split("\n\n");
 	const instructions = instructionsStr.split("");
@@ -24,6 +24,11 @@ export const part1 = (input) => {
 		.split("\n")
 		.map((row): [Node, string] => row.split(" = ") as [Node, string])
 		.map(formatNodeDirections);
+	return [instructions, nodes];
+};
+
+export const part1 = (input) => {
+	const [instructions, nodes] = parse(input);
 	let steps = 0;
 	let currentNode = nodes.find((n) => n[0] === "AAA");
 	let currentDirection = 0;
@@ -31,9 +36,7 @@ export const part1 = (input) => {
 	for (let i = 0; currentNode[0] !== "ZZZ"; i++) {
 		const instruction = instructions[currentDirection];
 		currentNode = nodes.find((n) => n[0] === currentNode[1][RL[instruction]]);
-
 		steps++;
-
 		// if we're at the last instruction, loop back to the beginning
 		if (currentDirection === instructions.length - 1) {
 			currentDirection = 0;
@@ -45,4 +48,37 @@ export const part1 = (input) => {
 	return steps;
 };
 
-export const part2 = (input) => {};
+const startingNodes = (nodes) => nodes.filter((n) => n[0].endsWith("A"));
+
+export const part2 = (input) => {
+	const [instructions, nodes] = parse(input);
+	let steps = 0;
+	let currentNodes = startingNodes(nodes);
+	const numOfNodes = currentNodes.length;
+	let currentDirection = 0;
+	for (let i = 0; !currentNodes.every((n) => n[0].endsWith("Z")); i++) {
+		const instruction = instructions[currentDirection];
+		const nextNodes = [];
+		nodes.forEach((n) => {
+			if (numOfNodes === nextNodes.length) {
+				return;
+			}
+			currentNodes.forEach((currentNode) => {
+				if (n[0] === currentNode[1][RL[instruction]]) {
+					nextNodes.push(n);
+				}
+			});
+		});
+		currentNodes = nextNodes;
+
+		steps++;
+		// if we're at the last instruction, loop back to the beginning
+		if (currentDirection === instructions.length - 1) {
+			currentDirection = 0;
+		} else {
+			currentDirection++;
+		}
+	}
+
+	return steps;
+};
